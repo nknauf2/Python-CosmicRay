d0cdeaa1c480d74c072c2071ff5b6880f18c30e7
nknauf2 <nknauf2@illinois.edu>
2016-08-11 13:56:50 -0500

    bug fix

--- geo.py
+++ geo.py
@@ -5,7 +5,8 @@ Authors: Luis Vasquez, Nate Knauf
 # will need to be modified to return geometry data for a range of dates, exceptions should be raised if flux data is analyzed without
 # existing geometry data
 
-def geoExtration(Filename):
+
+def geoExtraction(Filename):
     # prints geometry information. Returns a dictionary containing the entries 'Latitude','Longitude','Altitude',
     # 'Stacked', 'Areas'
 
@@ -14,13 +15,13 @@ def geoExtration(Filename):
         # print data, load dictionary to access arguments in other files
         lines = f.read().splitlines()
         print "Latitude: " + lines[1]
-        geo_dictionary['Latitude'] = float(lines[1])
+        geo_dictionary['Latitude'] = lines[1]
 
         print "Longitude: " + lines[2]
-        geo_dictionary['Longitude'] = float(lines[2])
+        geo_dictionary['Longitude'] = lines[2]
 
         print "Altitude: " + lines[3]
-        geo_dictionary['Altitude'] = float(lines[3])
+        geo_dictionary['Altitude'] = lines[3]
 
         if (lines[4] == "1"): # indicate if detectors are stacked
             print "Stacked"
@@ -36,3 +37,4 @@ def geoExtration(Filename):
         print "Areas:", Area
         geo_dictionary['Areas'] = Area
     return geo_dictionary
+
